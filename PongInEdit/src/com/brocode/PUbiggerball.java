ac5b4ecc35d03257a583e8ea5086afbfac2fc3a4
duevano fairuz <duevanofairuz@gmail.com>
2022-12-01 11:20:15 +0700

    Commit 01/12/22

---
+++ PongInEdit/src/com/brocode/PUbiggerball.java
@@ -0,0 +1,42 @@
+package com.brocode;
+
+import java.awt.*;
+import java.util.*;
+
+public class PUbiggerball extends Rectangle{
+
+	Random random;
+	int xVelocity;
+	int yVelocity;
+	int initialSpeed = 4;
+	
+	PUbiggerball(int x, int y, int width, int height){
+		super(x,y,width,height);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection*initialSpeed);
+		
+//		int randomYDirection = random.nextInt(2);
+//		if(randomYDirection == 0)
+//			randomYDirection--;
+//		setYDirection(randomYDirection*initialSpeed);
+		
+	}
+	
+	public void setXDirection(int randomXDirection) {
+		xVelocity = randomXDirection;
+	}
+//	public void setYDirection(int randomYDirection) {
+//		yVelocity = randomYDirection;
+//	}
+	public void move() {
+		x += xVelocity;
+		y += yVelocity;
+	}
+	public void draw(Graphics g) {
+		g.setColor(Color.pink);
+		g.fillRect(x, y, width, height);
+	}
+}
